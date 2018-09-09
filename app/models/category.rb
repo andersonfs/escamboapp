6d95932f3b26de194c2bce52bf68106a800dc4eb
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-09-08 23:08:43 -0300

    Configured counter cache for categories and ads.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+
+  # Associtations
+  has_many :ads
+
+  # Validations
   validates_presence_of :description
+
   # Scopes
   scope :order_by_description, -> { order(:description) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_08_172653) do
+ActiveRecord::Schema.define(version: 2018_09_09_014703) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_09_08_172653) do
     t.string "description", limit: 60
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "ads_count"
   end
 
   create_table "members", force: :cascade do |t|
