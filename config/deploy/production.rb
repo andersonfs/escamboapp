0a11e5f0b071d2d3a503ebb98e020f7577cc48ab
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-09-16 23:19:17 -0300

    Added unicorn on production capistrano file.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,12 @@ server "142.93.177.130", user: "deploy", roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+root = "/var/www/escamboapp/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+worker_processes 4
+timeout 30
+preload_app true
+listen '/tmp/escamboapp.sock', backlog: 64
