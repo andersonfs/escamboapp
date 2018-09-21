42d69985def35e92deeea2a9f8da28fa74f88f6e
Anderson Firmino <andersonfs@gmail.com>
2018-09-21 18:36:12 +0000

    Added Pagseguro payment controller.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: rails s -b 0.0.0.0 -p 3000
+web: RAILS_ENV=development rails s -b 0.0.0.0 -p 3000
 mailcatcher: mailcatcher --http-ip 0.0.0.0 -f -v
--- app/controllers/checkout/payments_controller.rb
+++ app/controllers/checkout/payments_controller.rb
@@ -1,7 +1,37 @@
 class Checkout::PaymentsController < ApplicationController
 
   def create
-    render text: "Processando..."
+    #Email: c31944527509479925357@sandbox.pagseguro.com.br
+    #Senha: 549734869Pk305dF
+    #Cartao: 4111111111111111 / Bandeira: VISA Válido até: 12/2030 CVV: 123
+
+    ad = Ad.find(params[:ad_id])
+    ad.processing!
+
+    order = Order.create(ad: ad, buyer_id: current_member.id )
+    order.waiting!
+
+    payment = PagSeguro::PaymentRequest.new
+    # Uma melhoria seria criar um campo id da transação
+    payment.reference = order.id
+    payment.notification_url = root_url # Fix Later
+    payment.redirect_url = site_ad_detail_url(ad)
+
+    payment.items << {
+        id: ad.id,
+        description: ad.title,
+        amount: ad.price.to_s.gsub(',','.')
+    }
+
+    response = payment.register
+
+    if response.errors.any?
+      redirect_to site_ad_detail_path(ad), alert: "Erro ao processar compra... Entre em contato com o SAC (21) 0800-9090"
+    else
+      redirect_to response.url
+    end
+
+    render text: "Processando.... Pedido: #{order.status_i18n} - Anúncio: #{ad.status_i18n}"
   end
 
 end
